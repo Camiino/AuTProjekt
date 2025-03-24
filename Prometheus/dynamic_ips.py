2aaf0b0fe380c8515a77e0e8d2c73d44d9ed8dd5
André Sers <48219248+sersandre@users.noreply.github.com>
2025-03-24 17:14:49 +0100

    Update dynamic_ips.py to work with network arp scan

--- Prometheus/dynamic_ips.py
+++ Prometheus/dynamic_ips.py
@@ -1,46 +1,35 @@
-import nmap
 import os
 import json
 import socket
-from apscheduler.schedulers.blocking import BlockingScheduler
+from scapy.all import srp,Ether,ARP,conf
 
-hostnames = {"eiba-dl", "eiba-dl2"}
-scheduler = BlockingScheduler()
+hostnames = {}
 
+def arp_scan(interface, ips):
+    found = {}
+    conf.verb = 0
+    ans, uans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ips),
+                    timeout = 2,
+                    iface = interface,
+                    inter = 0.1)
+    for snd,rcv in ans:
+        out = rcv.sprintf(r"%ARP.psrc% - %Ether.src%").split("-")
+        ip = out[0].strip()
+        mac = out[1].strip()
+        if mac in hostnames:
+            hostname = hostnames[mac]
+            found[hostname] = ip
 
-@scheduler.scheduled_job("interval", seconds=10)
-def get_devices():
-    devices = {}
-    for device in os.popen('arp -a'):
-        listing = device.split(" ")
-        hostname = listing[0].split(".")[0]
-        ip = listing[1].strip("()")
-        if hostname in hostnames:
-            devices[hostname] = f"{ip}:9100"
-    print(devices)
-    
-    json_out = []
+    return found
 
-    overview = {"labels": {
-                    "job": "overview"
-                },
-                "targets": [devices[hostname] for hostname in devices.keys()]}
-    json_out.append(overview)
-
-    with open("/home/sersandr/monitoring/overview.json", "w") as out:
-        json.dump(json_out, out, indent=4)
-    
-    for hostname in devices.keys():
+if __name__ == "__main__":
+    found = arp_scan("enp5s0f0", "172.17.21.1/24")
+    for hostname in found.keys():
         json_out = []
         singular = {"labels": {
                         "job": hostname
                     },
-                    "targets": [devices[hostname]]}
+                   "targets": [f"{found[hostname]}:8080"]}
         json_out.append(singular)
         with open(f"/home/sersandr/monitoring/{hostname}.json", "w") as out:
             json.dump(json_out, out, indent=4)
-
-
-if __name__ == "__main__":
-    scheduler.start()
-    
